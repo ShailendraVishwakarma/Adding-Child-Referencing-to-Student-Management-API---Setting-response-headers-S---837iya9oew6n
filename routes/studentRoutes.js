1c30d2418e2f907e1bccfa2982b200b8713e9ef8
ShailendraVishwakarma <110275464+ShailendraVishwakarma@users.noreply.github.com>
2023-05-13 21:14:06 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,9 +1,23 @@
-const express = require("express");
-
-const { getAllStudents } = require("../controllers/studentControllers");
-
+const express = require('express');
 const router = express.Router();
+const { Student } = require('../models/Model');
 
-router.get("/", getAllStudents);
+router.get('/students', async (req, res) => {
+  try {
+    const students = await Student.find().populate('subjects');
+    res.status(200).json({
+      status: 'success',
+      data: {
+        count: students.length,
+        students: students
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: err.message
+    });
+  }
+});
 
 module.exports = router;
